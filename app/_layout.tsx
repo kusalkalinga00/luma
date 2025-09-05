7fbda116d0a6af36877de940cfb68f286fb4ae39
Kusal Kalinga <kusalkalingainfo@gmail.com>
2025-09-05 20:10:54 +0530

    feat: Add protected and public layout components with authentication handling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,26 @@
-import { ClerkProvider } from "@clerk/clerk-expo";
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Stack } from "expo-router";
+
+function RootLayoutWithAuth() {
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) {
+    return null;
+  }
+
+  return (
+    <Stack>
+      <Stack.Protected guard={isSignedIn}>
+        <Stack.Screen name={"(protected)"} />
+      </Stack.Protected>
+
+      <Stack.Protected guard={!isSignedIn}>
+        <Stack.Screen name={"(public)"} />
+      </Stack.Protected>
+    </Stack>
+  );
+}
 
 export default function RootLayout() {
   return (
@@ -8,7 +28,7 @@ export default function RootLayout() {
       tokenCache={tokenCache}
       publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
     >
-      <Stack />;
+      <RootLayoutWithAuth />;
     </ClerkProvider>
   );
 }
