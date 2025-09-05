c087f88e751e62eda5b5035975b322c2daf5fd82
Kusal Kalinga <kusalkalingainfo@gmail.com>
2025-09-06 00:25:24 +0530

    refactor: Clean up JSX structure in Index component for better readability

--- app/(protected)/index.tsx
+++ app/(protected)/index.tsx
@@ -3,15 +3,18 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Home Screen</Text>
-      <SignOutButton />
-    </View>
+    <>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>Home Screen</Text>
+        <SignOutButton />
+      </View>
+      {/* <Gradient position="top" isSpeaking={true} /> */}
+    </>
   );
 }
